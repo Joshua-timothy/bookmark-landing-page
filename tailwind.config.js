abbe28ca7ee749ddf93abb93d8184fd60a323dc4
Joshua-timothy <Joshuatimothy.dev@gmail.com>
2023-12-18 01:15:47 +0100

    Added custom font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rubik: "'Rubik', sans-serif",
+      },
+      colors: {
+        SoftBlue: "hsl(231, 69%, 60%)",
+        SoftRed: "hsl(0, 94%, 66%)",
+        GrayishBlue: "hsl(229, 8%, 60%)",
+        VeryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+    },
   },
   plugins: [],
 };
